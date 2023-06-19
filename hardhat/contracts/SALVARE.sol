18c29d201d342a628f89e65c10650151264e18d8
yawn <69970183+yawn-c111@users.noreply.github.com>
2023-06-19 20:59:33 +0900

    feat: add salvare

---
+++ hardhat/contracts/SALVARE.sol
@@ -0,0 +1,262 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+contract SALVARE is ERC20 {
+    struct TrashCan {
+        uint256 id;
+        address trashCanOwner;
+        address trashCan;
+        string trashCanLocationAddress;
+        string trashCanLocationImage;
+        uint256 trashCanLatitude;
+        uint256 trashCanLongitude;
+        uint256 trashCanAmount;
+        uint256 trashCanMaxAmount;
+        uint256 trashCanReward;
+        RecyclingCenter recyclingCenter;
+    }
+
+    struct Work {
+        uint256 id;
+        uint256 gram;
+    }
+
+    struct RecyclingCenter {
+        uint256 id;
+        string recyclingCenterName;
+        string recyclingCenterLocationAddress;
+        uint256 recyclingCenterLatitude;
+        uint256 recyclingCenterLongitude;
+    }
+
+    event StartWork(address indexed worker, uint256 indexed id);
+    event DoneWork(address indexed worker, uint256 indexed id);
+    event Balance(address indexed worker, uint256 balance);
+
+    constructor() ERC20("SALVAREToken", "SALVARE") {
+        _initTrashCan();
+        _mint(address(this), 1000000 * 10 ** 18);
+    }
+
+    TrashCan[] trashCans;
+    mapping(address => Work) public workerToWork;
+
+    function _initTrashCan() private {
+        //ゴミ箱を登録
+        trashCans.push(
+            TrashCan(
+                0,
+                address(0x00),
+                address(0x00),
+                "",
+                "",
+                0,
+                0,
+                0,
+                0,
+                0,
+                RecyclingCenter(0, "", "", 0, 0)
+            )
+        ); //識別用のダミーを作成
+
+        trashCans.push(
+            TrashCan(
+                1,
+                address(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4),
+                address(0x00),
+                unicode"渋谷 ハチ公前",
+                "https://cdn.discordapp.com/attachments/1087354845675126786/1115645639053807738/image.png",
+                3565834639713606,
+                13970303583403240,
+                10000,
+                20000,
+                0,
+                RecyclingCenter(
+                    0,
+                    unicode"渋谷清掃工場",
+                    unicode"〒150-0011 東京都渋谷区東１丁目３５−１",
+                    3565346025058248,
+                    13970648889528675
+                )
+            )
+        ); //識別用のダミーを作成
+
+        trashCans.push(
+            TrashCan(
+                2,
+                address(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4),
+                address(0x00),
+                unicode"Shibuya-ku Address 2",
+                "https://cdn.discordapp.com/attachments/1087354845675126786/1115645639053807738/image.png",
+                3565876211991804,
+                13970375443275780,
+                11000,
+                20000,
+                0,
+                RecyclingCenter(
+                    0,
+                    unicode"渋谷清掃工場",
+                    unicode"〒150-0011 東京都渋谷区東１丁目３５−１",
+                    3565346025058248,
+                    13970648889528675
+                )
+            )
+        ); //識別用のダミーを作成
+
+        trashCans.push(
+            TrashCan(
+                3,
+                address(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4),
+                address(0x00),
+                unicode"Shibuya-ku Address 3",
+                "https://cdn.discordapp.com/attachments/1087354845675126786/1115645639053807738/image.png",
+                3565836740592249,
+                13970355092249920,
+                1200,
+                20000,
+                0,
+                RecyclingCenter(
+                    0,
+                    unicode"渋谷清掃工場",
+                    unicode"〒150-0011 東京都渋谷区東１丁目３５−１",
+                    3565346025058248,
+                    13970648889528675
+                )
+            )
+        ); //識別用のダミーを作成
+
+        trashCans.push(
+            TrashCan(
+                4,
+                address(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4),
+                address(0x00),
+                unicode"Shibuya-ku Address 4",
+                "https://cdn.discordapp.com/attachments/1087354845675126786/1115645639053807738/image.png",
+                3565889013484929,
+                13969978926521935,
+                13000,
+                20000,
+                0,
+                RecyclingCenter(
+                    0,
+                    unicode"渋谷清掃工場",
+                    unicode"〒150-0011 東京都渋谷区東１丁目３５−１",
+                    3565346025058248,
+                    13970648889528675
+                )
+            )
+        ); //識別用のダミーを作成
+
+        trashCans.push(
+            TrashCan(
+                5,
+                address(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4),
+                address(0x00),
+                unicode"Shibuya-ku Address 5",
+                "https://cdn.discordapp.com/attachments/1087354845675126786/1115645639053807738/image.png",
+                3565774332711117,
+                13969982208945464,
+                14000,
+                20000,
+                0,
+                RecyclingCenter(
+                    0,
+                    unicode"渋谷清掃工場",
+                    unicode"〒150-0011 東京都渋谷区東１丁目３５−１",
+                    3565346025058248,
+                    13970648889528675
+                )
+            )
+        ); //識別用のダミーを作成
+
+        trashCans.push(
+            TrashCan(
+                6,
+                address(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4),
+                address(0x00),
+                unicode"Shibuya-ku Address 6",
+                "https://cdn.discordapp.com/attachments/1087354845675126786/1115645639053807738/image.png",
+                3565798863813731,
+                13970174987485640,
+                15000,
+                20000,
+                0,
+                RecyclingCenter(
+                    0,
+                    unicode"渋谷清掃工場",
+                    unicode"〒150-0011 東京都渋谷区東１丁目３５−１",
+                    3565346025058248,
+                    13970648889528675
+                )
+            )
+        ); //識別用のダミーを作成trashCanList[msg.sender]=1;
+    }
+
+    function isWorking(address worker) public view returns (Work memory) {
+        return workerToWork[worker];
+    }
+
+    function setAmountOfTrash(uint256 _newAmount, uint256 id) public {
+        //ゴミ箱からのトランザクションのみ受け付ける
+        //require(workerToWork[msg.sender]==0,"you are not trash can");
+        trashCans[id].trashCanAmount = _newAmount;
+    }
+
+    function getTrashCans() public view returns (TrashCan[] memory) {
+        return trashCans;
+    }
+
+    function getWork(address target) public view returns (Work memory) {
+        return workerToWork[target];
+    }
+
+    function verifySignature(
+        bytes32 messageHash,
+        bytes memory signature
+    ) public pure returns (address) {
+        return
+            ECDSA.recover(ECDSA.toEthSignedMessageHash(messageHash), signature);
+    }
+
+    function startWork(
+        uint256 trashWeight,
+        bytes32 messageHash,
+        bytes memory signature,
+        uint256 id
+    ) public {
+        //ゴミ箱からのトランザクションのみ受け付ける
+        //署名を検証しワーカーのアドレスを取得
+        //is_workに運んでいる量を記録 mapping (address=>[uint 256,uint 256]) work
+        address worker = verifySignature(messageHash, signature);
+        // trashWeight != 0 -> isWorking
+        workerToWork[worker].gram = trashWeight;
+        workerToWork[worker].id = id;
+
+        trashCans[id].trashCanAmount -= trashWeight;
+        emit StartWork(worker, id);
+    }
+
+    function doneWork(
+        uint actualWeight,
+        bytes32 messageHash,
+        bytes memory signature
+    ) public {
+        //集積所からのトランザクションを受け付ける
+        //署名を検証しワーカーのアドレスを取得
+        address worker = verifySignature(messageHash, signature);
+        //is_workに運んでいる量と報酬を記録 mapping (address=>[uint 256,uint 256]) work
+        uint256 trashWeight = workerToWork[worker].gram;
+        //actualWeight is weight of trash on Recycle center 500g
+        //trashWeight is weight of trash brought by Worker 1000g
+        emit DoneWork(worker, workerToWork[worker].id);
+        emit Balance(worker, balanceOf(worker) + actualWeight * 10 ** 18);
+        workerToWork[worker].gram = 0;
+        workerToWork[worker].id = 0;
+
+        if (trashWeight - trashWeight / 10 < actualWeight) {
+            _transfer(address(this), worker, actualWeight * 10 ** 18);
+        }
+    }
+}
--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,32 +1,84 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers, run, network } = require("hardhat");
+const fs = require("fs");
+
+const { TEST_NET_CHAIN_ID } = require("../helpers/constants");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const network = await ethers.provider.getNetwork();
+  console.log("network chainId:", network.chainId);
+  console.log("network name:", network.name);
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  let salvare;
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  // Deploy SALVARE contract
+  const SALVARE = await ethers.getContractFactory("SALVARE");
+  salvare = await SALVARE.deploy();
+  await salvare.deployed();
+  console.log("SALVARE deployed to:", salvare.address);
 
-  await lock.waitForDeployment();
+  if (network.chainId === TEST_NET_CHAIN_ID) {
+    console.log("Start verifying contracts...");
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+    console.log("Waiting for 10 seconds before verification...");
+    await new Promise((resolve) => setTimeout(resolve, 10000));
+    console.log("==========================");
+    console.log("Verifying SALVARE...");
+    try {
+      await run("verify:verify", {
+        address: salvare.address,
+        constructorArguments: [],
+      });
+    } catch (e) {
+      console.log("Failed to verify SALVARE");
+      console.log(e);
+    }
+
+    console.log("==========================");
+  }
+
+  console.log("Recording SALVARE...");
+  recording("SALVARE", salvare);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+const recording = async (contractName, contract) => {
+  let data;
+  let networkName = network.name ? network.name : "undefined";
+  try {
+    data = JSON.parse(
+      fs.readFileSync(
+        `./contracts-data/${networkName}/${contractName}-data.json`
+      )
+    );
+  } catch (e) {
+    data = [];
+  }
+
+  now = new Date();
+  const dateTimeJpn =
+    now.getFullYear() +
+    "/" +
+    now.getMonth() +
+    "/" +
+    now.getDate() +
+    "/" +
+    now.getHours() +
+    ":" +
+    now.getMinutes() +
+    ":" +
+    now.getSeconds();
+
+  await data.unshift({
+    name: contractName,
+    address: contract.address,
+    time: dateTimeJpn,
+  });
+
+  fs.writeFileSync(
+    `./contracts-data/${networkName}/${contractName}-data.json`,
+    JSON.stringify(data, null, 2)
+  );
+};
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
