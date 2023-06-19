203449294092a03516f9509ab11415a6c925d7fc
CanIyu <35588557+CanIyu@users.noreply.github.com>
2023-06-19 20:08:51 +0900

    add sign page

--- salvare/components/Sign.js
+++ salvare/components/Sign.js
@@ -32,14 +32,13 @@ export default function Sign() {
   if (isVerified == true) {
     return (
       <>
-        <div>Verified!</div>
       </>
     )
   } else {
     return (
       <>
         <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', backgroundImage: 'linear-gradient(to bottom, rgba(0, 199, 83, 0.9), rgba(0, 255, 255, 0.9))', zIndex: 9999 }}>
-          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontSize: '34px' }}>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontSize: '34px', fontWeight: "bold" }}>
             <div onClick={onClick}>ここを押してウォレットを接続してください</div>
           </div>
         </div>
--- salvare/src/pages/index.js
+++ salvare/src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+// import ShowPins from "../../components/ShowPins";
+// import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,7 @@ export default function Home() {
       {/* <main className={`${styles.main} ${inter.className}`}> */}
       <main>
         <h1>Home</h1>
+        {/* <ShowPins cont={props.cont} /> */}
       </main>
     </>
   )
